de065193f016c9df76975365c518c0c13c8a3fe5
Harish <bhattaharish10@gmail.com>
2024-11-18 18:33:43 +0545

    Get Single Post

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,11 +1,18 @@
 import { Post } from "../entities/Post";
 import { MyContext } from "src/types";
-import { Ctx, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
   posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
+  }
+  @Query(() => Post, { nullable: true }) // This is the graphql type. Return type Post or null. We cannot do Post | null
+  post(
+    @Arg("id", () => Int) id: number, // We can put anthing instead of "identifier" which will be used when querying the graphql
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    return em.findOne(Post, { id });
   }
 }
